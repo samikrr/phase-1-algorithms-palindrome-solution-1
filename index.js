a17a0d382fdadbda9d3617792c39a404e857fa02
Samuel Kirori <samuelkirori300@gmail.com>
2024-04-17 17:12:19 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  ord = word.toLowerCase();
+  const reversedWord = reverse(word);
+  
+  return word === reversedWord;
+}
+
+
+function reverse(word){
+  //'abc' => 'cba'
+  // const wordArray = word.split("");
+  // const reverserdWordArray = wordArray.reverse();
+  // const reversedWord = reverserdWordArray.join("");
+  // return reversedWord;
+return word.split("").reverse().join("");
 }
 
 /* 
@@ -21,5 +35,12 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("ab"));
+
+
+
 
 module.exports = isPalindrome;
